d1442da2479b94c79bc20a048e2ea4fad9b0931c
xuman <xuman@list.ru>
2018-07-17 20:14:31 +0300

    Upgraded App component to present actual application

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,7 +1,25 @@
+import { observer } from 'mobx-react'
 import * as React from 'react'
 
-export class App extends React.PureComponent {
+import { Add } from '../../components/Add'
+import { IAppStore } from '../../stores/AppStore'
+
+@observer
+export class App extends React.Component<{store: IAppStore}> {
   render() {
-    return <h1>This App is stupid for now! But things will change with time!</h1>
+    const numbersTemplate = this.props.store.numbers.map(item => {
+      return <li key={item.number}>{item.funNumberString}</li>
+    })
+
+    return (
+      <div>
+        <h1>This App is stupid for now! But things will change with time!</h1>
+        <Add addNumber={this.props.store.addNumber} />
+
+        <div>
+          {numbersTemplate}
+        </div>
+      </div>
+    )
   }
 }
