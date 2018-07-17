65872b51586a657b71284fb7187915edf7792dca
xuman <xuman@list.ru>
2018-07-17 20:15:29 +0300

    Updated App render

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 
-import App from './containers/App'
+import { App } from './containers/App'
+import { AppStore } from './stores/AppStore'
+
+const appStore = new AppStore()
 
 render(
-  <App />,
+  <App store={appStore} />,
   document.getElementById('app'),
 )
