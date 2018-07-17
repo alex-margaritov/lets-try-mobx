90513d95ffa7d22157cb739527ce812b6ae4bfe8
xuman <xuman@list.ru>
2018-07-17 15:43:54 +0300

    Initial dev enviroment and basic application

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export class App extends React.PureComponent {
+  render() {
+    return <h1>This App is stupid for now! But things will change with time!</h1>
+  }
+}
